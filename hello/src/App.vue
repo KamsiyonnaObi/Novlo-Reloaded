7211a87b2e14307577fed1a2db6a768cba949178
Dare Alesh <Damilarealesh99@gmail.com>
2022-06-23 02:14:09 -0500

    nav bar

--- hello/src/App.vue
+++ hello/src/App.vue
@@ -1,19 +1,28 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/NOVLO.png">
-    <HelloWorld msg="Welcome to Novlo Reloaded Page"/>
-  </div>
+  <v-app>
+    <v-app-bar color="navigation" flat>
+      <v-container fluid>
+        <v-row class="justify-space-between">
+          <v-col cols="12" class="d-flex align-center justify-center">
+            <v-btn color="primary" exact text large dark> Home </v-btn>
+            <v-btn color="primary" text large dark> About </v-btn>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-app-bar>
+    <HelloWorld msg="Welcome to Novlo Reloaded Page" />
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HelloWorld from "./components/HelloWorld.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  data: () => ({}),
+};
 </script>
 
 <style>
