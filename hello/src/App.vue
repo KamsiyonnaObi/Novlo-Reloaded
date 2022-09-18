4ff67305b6ccd4593f636ea527f62d3374a998ed
kamsi <obikamsi@gmail.com>
2022-09-17 20:50:55 -0500

    p

--- hello/src/App.vue
+++ hello/src/App.vue
@@ -1,16 +1,41 @@
 <template>
   <v-app>
-    <v-app-bar class="custom-nav-bar" flat app position="sticky">
+    <v-app-bar 
+      class="custom-nav-bar" 
+      flat app position="sticky" 
+      color="black"
+      dark
+      shrink-on-scroll
+      prominent
+      scroll-target="#scrolling-techniques"
+      >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+  
+        <v-toolbar-title>Title</v-toolbar-title>
+  
+        <v-spacer></v-spacer>
+  
+        <v-btn icon>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+  
+        <v-btn icon>
+          <v-icon>mdi-heart</v-icon>
+        </v-btn>
+  
+        <v-btn icon>
+          <v-icon>mdi-dots-vertical</v-icon>
+        </v-btn>
       <v-container>
         <v-row class="justify-space-between">
           <v-col cols="12" class="d-flex align-center justify-center">
             <v-img
-              max-height="100"
-              max-width="100"
+              max-height="250"
+              max-width="250"
               :src="require('./assets/Novlo2.jpg')"
             ></v-img>
-            <v-btn color="primary" exact text large dark> Home </v-btn>
-            <v-btn color="primary" text large dark> About </v-btn>
+            <v-btn color="#0323cb" exact text large dark> Home </v-btn>
+            <v-btn color="#0323cb" text large dark> About </v-btn>
           </v-col>
         </v-row>
       </v-container>
