888931d974ae3ee27c4b6b3abb9f8b5d73b6398c
Dare Alesh <damilarealesh99@gmail.com>
2022-09-17 19:56:32 -0500

    commit

--- hello/src/App.vue
+++ hello/src/App.vue
@@ -1,16 +1,23 @@
 <template>
   <v-app>
-    <v-app-bar color="navigation" flat>
-      <v-container fluid>
+    <v-app-bar class="custom-nav-bar" flat app position="sticky">
+      <v-container>
         <v-row class="justify-space-between">
           <v-col cols="12" class="d-flex align-center justify-center">
+            <v-img
+              max-height="100"
+              max-width="100"
+              :src="require('./assets/Novlo2.jpg')"
+            ></v-img>
             <v-btn color="primary" exact text large dark> Home </v-btn>
             <v-btn color="primary" text large dark> About </v-btn>
           </v-col>
         </v-row>
       </v-container>
     </v-app-bar>
-    <HelloWorld msg="Welcome to Novlo Reloaded Page" />
+    <v-main>
+      <HelloWorld msg="Welcome to Novlo Reloaded Page" />
+    </v-main>
   </v-app>
 </template>
 
@@ -27,11 +34,7 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
